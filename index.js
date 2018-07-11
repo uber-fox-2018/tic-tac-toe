742ae36cab6a2f9ad2faf541530ee320bbd64259
joanlamrack <joanlamrack@gmail.com>
2018-07-11 15:04:18 +0700

    fixed code

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ function tictactoe(){
 	for(let i = 0 ; i < board.length ; i++){
 		for(let j = 0 ; j < board.length ; j++){
 			let player = Math.round(Math.random());
-			if(player && (xSum <= 4 || xSum <=5)){
+			if(player && (xSum <= 4 || xSum <= 5)){
 				board[i][j] = "X";
 				xSum++;
 			}
 			else{
-				board[i][j] = "O";
-				oSum++;
+				if(oSum <= (4||5)){
+					board[i][j] = "O";
+					oSum++;
+				}
+				
+				
 			}
 
 		}
