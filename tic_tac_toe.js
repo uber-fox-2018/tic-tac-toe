61f3506c406bc4451c4402138d8ec76fe29169a4
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-11 21:16:57 +0700

    Fixing Tic Tac Toe

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,30 +1,34 @@
 function ticTacToe (row, column) {
   let result = [];
   let countX = 0;
+  let countO = 0;
+  let variabel = 'XO';
   // let countO = 0;
   for (let i = 0; i < row; i++) {
     let container = [];
     for (let j = 0; j < row; j++) {
-      if (countX <= 4) {
-        if(j % 2 !== 0) {
-          countX += 1;
-          container.push('X');
-        } else if (i % 4 === 0){
-          container.push('O');
-        } else {
-          container.push('X');
-          countX += 1;
-        }
-      } else {
-        if(i % 2 === 0) {
-          container.push('O');
-        }
+      let randomOX = variabel[Math.floor(variabel.length * Math.random())];
+      if (randomOX === 'X' && countX != 5) {
+        container.push(randomOX);
+        countX ++;
+      } else if (randomOX === 'O' && countO != 5){
+        container.push(randomOX);
+        countO ++
+      } else if(countO >= 5) {
+        container.push('X')
+        countX++
+      } else if(countX >= 5) {
+        container.push('O')
+        countO++
       }
-      // container.push(i);
+      else {
+        j-= 1;
+      }
     }
     result.push(container);
   }
   return result;
+  console.log(countO);
 }
 
 console.log(ticTacToe(3, 3));
